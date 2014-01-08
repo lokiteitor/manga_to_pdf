b27fd1a68d10ecbe40fe4e2174294c537f6a4ff1
lokiteitor <lokiteitor513@gmail.com>
2014-01-07 23:40:59 -0600

    script de empaquetado en proceso

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2.7
+from distutils.core import setup
+
+
+setup(
+    name='manga_to_pdf'
+    , url='http://www.desarrolloslkt.tk'
+    , author='David Delgado'
+    , author_email='lokiteitor513@gmail.com'
+    , version='1.1.0' # remember to change me for new versions!
+    , description='sencillo programa que gestina el manga del usuario de forma totalmente autonoma'
+    , long_description=open('README.txt').read()
+    , scripts=['mangatopdf.py']
+    , packages=['Filemanga']
+    , package_data = {'pymunk': ['chipmunk.dll'
+                                , 'chipmunk64.dll'
+                                , 'libchipmunk.so'
+                                , 'libchipmunk64.so'
+                                , 'libchipmunk.dylib']}
+    , license='GPL'
+
+    )
