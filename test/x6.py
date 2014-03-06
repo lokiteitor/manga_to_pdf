6d5b2a30116736d559482295b83accfc27718c3c
lokiteitor <lokiteitor513@gmail.com>
2014-03-06 17:26:55 -0600

    mejorado y simplificado

--- test/x6.py
+++ test/x6.py
@@ -17,32 +17,10 @@ def order(dsd):
     print lst
 
 
-
 lst = [5,3,8,4,1,12,15,14,0,2,6]
-
-
-
-aqui = 0
-min = 100000
-lon = 0
-while lon < len(lst):
-
-    if lst[lon] < min:
-        min = lst[lon]
-    lon = lon + 1
-    print min
-
-lst.remove(min)
-
-lst.insert(aqui,min)
-
-
-
-
 print lst
 
 
-
-for i in range(len(lst)+1):
+for i in range(len(lst)):
 
     order(i)
