54ca8a99df58b1d60c1eb3e2964ad280cfadfddd
lokiteitor <lokiteitor513@gmail.com>
2014-03-06 17:21:30 -0600

    prueba de algoritmo de ordenamiento por seleccion

---
+++ test/x6.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2.7
+
+def order(dsd):
+    aqui = dsd
+    min = 100000
+    lon = dsd
+    while lon < len(lst):
+
+        if lst[lon] < min:
+            min = lst[lon]
+        lon = lon + 1
+        print min
+    if min != 100000:
+        lst.remove(min)
+        lst.insert(aqui,min)
+
+    print lst
+
+
+
+lst = [5,3,8,4,1,12,15,14,0,2,6]
+
+
+
+aqui = 0
+min = 100000
+lon = 0
+while lon < len(lst):
+
+    if lst[lon] < min:
+        min = lst[lon]
+    lon = lon + 1
+    print min
+
+lst.remove(min)
+
+lst.insert(aqui,min)
+
+
+
+
+print lst
+
+
+
+for i in range(len(lst)+1):
+
+    order(i)
